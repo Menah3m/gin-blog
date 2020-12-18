8080e2f3cd8f6f7db5d16a6fd81945438f12e6dc
yasu <568200065@qq.com>
2020-12-18 19:46:29 +0900

    update: article part

--- routers/api/v1/article.go
+++ routers/api/v1/article.go
@@ -58,7 +58,7 @@ func GetArticles(c *gin.Context)  {
 
 	var tagId int = -1
 	if arg := c.Query("tag_id"); arg != "" {
-		tagID = com.StrTo(arg).MustInt()
+		tagId = com.StrTo(arg).MustInt()
 		maps["tag_id"] = tagId
 
 		valid.Min(tagId, 1, "tag_id").Message("标签Id必须大于0")
@@ -91,7 +91,7 @@ func AddArticle(c *gin.Context)  {
 	desc := c.Query("desc")
 	content := c.Query("content")
 	createdBy := c.Query("created_by")
-	state := com.StrTo(c.DefaultQuery("state", 0)).MustInt()
+	state := com.StrTo(c.DefaultQuery("state", "0")).MustInt()
 
 	valid := validation.Validation{}
 	valid.Min(tagId, 1, "tag_id").Message("标签ID必须大于0")
@@ -198,5 +198,28 @@ func EditArticle(c *gin.Context)  {
 // TODO
 // 删除文章
 func DeleteArticle(c *gin.Context)  {
+	id := com.StrTo(c.Param("id")).MustInt()
 
+	valid := validation.Validation{}
+	valid.Min(id, 1, "id").Message("ID必须大于0")
+
+	code := e.INVALID_PARAMS
+	if ! valid.HasErrors(){
+		if models.ExistArticleByID(id){
+			models.DeleteArticle(id)
+			code = e.SUCCESS
+		} else {
+			code = e.ERROR_NOT_EXIST_ARTICLE
+		}
+	} else {
+		for _, err := range valid.Errors{
+			log.Printf("err.key: %s, err.message: %s", err.Key, err.Message)
+		}
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"code": code,
+		"msg": e.GetMsg(code),
+		"data": make(map[string]string),
+	})
 }
